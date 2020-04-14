106053c565a04f80d1b47e8b6f68195054ba0cfe
Michael Lackey <michael.g.lackey@gmail.com>
2020-04-14 10:36:10 -0500

    Updates

---
+++ precache-manifest.e5ede5cad5afdb54d08e14aaed38473a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6501c78e84279baf2251c77f022782",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "688443fa45a5afe8d6e4",
+    "url": "/project-4-assessment/static/css/main.2a768a0f.chunk.css"
+  },
+  {
+    "revision": "bf9faed5984ec60f7781",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.36c0ca1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688443fa45a5afe8d6e4",
+    "url": "/project-4-assessment/static/js/main.8315615e.chunk.js"
+  },
+  {
+    "revision": "bb8b828653ad1446ab40",
+    "url": "/project-4-assessment/static/js/runtime-main.58f97c80.js"
+  }
+]);
